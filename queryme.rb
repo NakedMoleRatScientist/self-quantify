2e96357763d647b758d8cacee7e64f4990edcb1b
Han Dao <hackerkiba@gmail.com>
2012-09-27 21:27:08 -0400

    move into classes

--- queryme.rb
+++ queryme.rb
@@ -2,13 +2,13 @@ require"rubygems"
 require"date"
 require"csv"
 
-class Query
+class DataLog
+  attr_reader :dates, :steps, :weights, :times
   def initialize 
     @dates = []
     @steps = []
     @weights = []
     @times = []
-    @modes = []
     
     CSV.foreach("quantified.csv") do |line|
       @dates << line[0]
@@ -23,32 +23,54 @@ class Query
       print w.to_s + " , "
     end
   end
+
 end
 
 
-def announce_date dates
-  if dates.last == Date.today.to_s
-    "Today"
-  else
-    "As of the date " + dates.last
+
+class AnnouceToday
+  def initialize 
+    @data = DataLog.new()
   end
+
+  def date 
+    if @data.dates.last == Date.today.to_s
+      "Today"
+    else
+      "As of the date " + dates.last
+    end
+  end
+
+  def step
+    if @data.steps.last.nil?
+      ", there were no steps data logged."
+    else
+      ", I logged " + steps.last + " steps."
+    end
+  end
+
+  def weight
+    if @data.weights.last.nil?
+      " No weight data was logged."
+    else
+      " I weighed " + weights.last + " pounds "
+    end
+  end
+
+  def time 
+    if @data.times.last.nil?
+      return
+    else
+      "on " + times.last.to_s + "."
+    end
+  end
+
+  def announce 
+    puts date + step + weight + time
+  end
+  
 end
 
-def announce_walk steps
-  if steps.last.nil?
-    ", there was no steps data logged."
-  else
-    ", I logged " + steps.last + " steps."
-  end
-end
-
-def announce_weight weights
-  if weights.last.nil?
-    " No weight data was logged."
-  else
-    " I weighed " + weights.last + " pounds "
-  end
-end
 
 def announce_time times, modes
   if times.last.nil?
