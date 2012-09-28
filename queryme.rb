18f78de507f63cd557c153f100926ba796233bfd
Han Dao <hackerkiba@gmail.com>
2012-09-27 21:27:22 -0400

    remove clutter

--- queryme.rb
+++ queryme.rb
@@ -72,20 +72,6 @@ class AnnouceToday
 end
 
 
-def announce_time times, modes
-  if times.last.nil?
-    return
-  else
-    "on " + times.last.to_s + modes.last.to_s + "."
-  end
-end
-
-def how_many_days? dates
-  dates.size - 1
-end
-
-
-
 if ARGV[0] == "today"
   puts announce_date(dates) + announce_walk(steps) + announce_weight(weights) + announce_time(times,modes)
 elsif ARGV[0] == "weights"
